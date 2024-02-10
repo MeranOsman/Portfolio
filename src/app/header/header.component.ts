d33c24ce6b112ebe15694055382e3e4159f4f000
Meran Osman <meran-osman@gmx.de>
2024-02-10 16:50:33 +0100

    responsive introduction

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  constructor(private elementRef: ElementRef) { }
 
+  @ViewChild('menu_checkbox') menu_checkbox: any;
+  mobileMenu = false;
+
+
+  showMobileMenu() {
+    if (this.mobileMenu) {
+      this.mobileMenu = false;
+      document.body.style.overflowY = 'unset';
+    }
+    else if (!this.mobileMenu) {
+      this.mobileMenu = true;
+      document.body.style.overflowY = 'hidden';
+    }
+  }
+
+
+  closeMobileMenu() {
+    this.mobileMenu = false;
+    document.body.style.overflowY = 'unset';
+  }
 }
