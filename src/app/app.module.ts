c35e417795adb5350c7259c76a6f6b93aeb2c76f
Meran Osman <meran-osman@gmx.de>
2024-02-20 21:39:18 +0100

    current

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
+
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
--- src/app/main-website/contact/contactform/contactform.component.ts
+++ src/app/main-website/contact/contactform/contactform.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import { FormControl, Validators } from '@angular/forms';
+import { AbstractControl, ValidationErrors } from '@angular/forms';
 
 
 @Component({
@@ -10,6 +12,8 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ContactformComponent {
   constructor(private http: HttpClient) { }
+
+  emailForm = new FormControl('', [Validators.required, this.emailValidator]);
 
   contactData = {
     name: '',
@@ -53,5 +57,15 @@ export class ContactformComponent {
       left: 0,
       behavior: "auto"
     });
+  }
+
+
+  emailValidator(control: AbstractControl): ValidationErrors | null {
+    const email = control.value;
+    const pattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (email && !pattern.test(email)) {
+      return { invalidEmail: true };
+    }
+    return null;
   }
 }
